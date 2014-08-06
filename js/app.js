97170adab1ef8e99dcf6e9ab14741face6062e5f
Chris Alexander <chris@chris-alexander.co.uk>
2014-08-06 14:10:06 +0100

    First pass at compass implementation

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var compass = angular.module("compass",  ["ca-helpers"]);
+
+angular.module("compass").controller("compass", function($scope) {
+
+  $scope.angle = 0;
+  $scope.roundedAngle = 0;
+
+  var handleOrientationChange = function(e) {
+    $scope.angle = e.alpha;
+    $scope.roundedAngle = Math.round($scope.angle);
+    $scope.apply();
+  }
+
+  window.addEventListener('deviceorientation', handleOrientationChange);
+
+});
